726e836cc845c6e06c93f7644963765a55040559
harshit shukla <hharshit8118@gmail.com>
2023-06-26 12:24:44 +0530

    home page complete

--- src/components/listitem/Listitem.jsx
+++ src/components/listitem/Listitem.jsx
@@ -1,11 +1,51 @@
-import './listitem.scss'
+import {
+  Add,
+  PlayArrow,
+  ThumbDownAltOutlined,
+  ThumbUpAltOutlined,
+} from "@mui/icons-material";
+import "./listitem.scss";
+import { useState } from "react";
+const vid = require('./video.mp4')
 
-const Listitem = () => {
+const Listitem = ({ index }) => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className='listitem'>
-      <img src="https://images.unsplash.com/photo-1682687220161-e3e7388e4fad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="" />
+    <div
+      className="listitem"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img
+        src="https://images.unsplash.com/photo-1682687220161-e3e7388e4fad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+        alt=""
+      />
+      {isHovered && (
+        <>
+          <video src={vid} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className="icon"/>
+              <Add className="icon"/>
+              <ThumbUpAltOutlined className="icon"/>
+              <ThumbDownAltOutlined className="icon"/>
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+          </div>
+          <div className="desc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam quia
+            unde cumque, suscipit odit debitis.
+          </div>
+          <div className="genre">Action</div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Listitem
+export default Listitem;
