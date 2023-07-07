645bf67ca83687f1a1e71834dba59fde5c1e43ee
harshit shukla <hharshit8118@gmail.com>
2023-07-08 03:47:41 +0530

    add links to pages

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,19 @@
-import './sidebar.scss'
-import { AttachMoneyOutlined, Email, ForumOutlined, LineStyle, ManageAccounts, MessageOutlined, PersonOutlineOutlined, Report, ShoppingCart, Timeline, TrendingUp } from "@mui/icons-material";
+import "./sidebar.scss";
+import {
+  AttachMoneyOutlined,
+  Email,
+  ForumOutlined,
+  LineStyle,
+  ManageAccounts,
+  MessageOutlined,
+  PersonOutlineOutlined,
+  Report,
+  ShoppingCart,
+  Timeline,
+  TrendingUp,
+} from "@mui/icons-material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
@@ -9,16 +22,18 @@ const Sidebar = () => {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <div className="sidebarList">
+            <Link to={"/"} className="link">
+              <li className="sidebarListItem">
+                <LineStyle className="sidebarIcons" />
+                Home
+              </li>
+            </Link>
             <li className="sidebarListItem">
-              <LineStyle className='sidebarIcons'/>
-              Home
-            </li>
-            <li className="sidebarListItem">
-              <Timeline className='sidebarIcons'/>
+              <Timeline className="sidebarIcons" />
               Analytics
             </li>
             <li className="sidebarListItem">
-              <TrendingUp className='sidebarIcons'/>
+              <TrendingUp className="sidebarIcons" />
               Sales
             </li>
           </div>
@@ -26,20 +41,24 @@ const Sidebar = () => {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Quick Menu</h3>
           <div className="sidebarList">
+            <Link to="/users" className="link">
+              <li className="sidebarListItem">
+                <PersonOutlineOutlined className="sidebarIcons" />
+                Users
+              </li>
+            </Link>
+            <Link to="/products" className="link">
+              <li className="sidebarListItem">
+                <ShoppingCart className="sidebarIcons" />
+                Products
+              </li>
+            </Link>
             <li className="sidebarListItem">
-              <PersonOutlineOutlined className='sidebarIcons'/>
-              Users
-            </li>
-            <li className="sidebarListItem">
-              <ShoppingCart className='sidebarIcons'/>
-              Products
-            </li>
-            <li className="sidebarListItem">
-              <AttachMoneyOutlined className='sidebarIcons'/>
+              <AttachMoneyOutlined className="sidebarIcons" />
               Transactions
             </li>
             <li className="sidebarListItem">
-              <Report className='sidebarIcons'/>
+              <Report className="sidebarIcons" />
               Reports
             </li>
           </div>
@@ -48,15 +67,15 @@ const Sidebar = () => {
           <h3 className="sidebarTitle">Notifications</h3>
           <div className="sidebarList">
             <li className="sidebarListItem">
-              <Email className='sidebarIcons'/>
+              <Email className="sidebarIcons" />
               Mail
             </li>
             <li className="sidebarListItem">
-              <ForumOutlined className='sidebarIcons'/>
+              <ForumOutlined className="sidebarIcons" />
               Feedback
             </li>
             <li className="sidebarListItem">
-              <MessageOutlined className='sidebarIcons'/>
+              <MessageOutlined className="sidebarIcons" />
               Messages
             </li>
           </div>
@@ -65,15 +84,15 @@ const Sidebar = () => {
           <h3 className="sidebarTitle">Staff</h3>
           <div className="sidebarList">
             <li className="sidebarListItem">
-              <ManageAccounts className='sidebarIcons'/>
+              <ManageAccounts className="sidebarIcons" />
               Manage
             </li>
             <li className="sidebarListItem">
-              <Timeline className='sidebarIcons'/>
+              <Timeline className="sidebarIcons" />
               Analytics
             </li>
             <li className="sidebarListItem">
-              <Report className='sidebarIcons'/>
+              <Report className="sidebarIcons" />
               Reports
             </li>
           </div>
