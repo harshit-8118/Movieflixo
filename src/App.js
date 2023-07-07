9e28ab7f44dab0af092102bfe7c160d7bf0e87a7
harshit shukla <hharshit8118@gmail.com>
2023-07-08 03:48:02 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -5,23 +5,30 @@ import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
+import NewUser from "./pages/newuser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newproduct/NewProduct";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
       <div className="container">
-        <Sidebar />
-        <div className="others">
-          <Router>
+        <Router>
+          <Sidebar />
+          <div className="others">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/users" element={<UserList />} />
               <Route path="/user/:userId" element={<User />} />
-              
+              <Route path="/newUser" element={<NewUser />} />
+              <Route path="/products" element={<ProductList />} />
+              <Route path="/product/:productId" element={<Product />} />
+              <Route path="/newProduct" element={<NewProduct />} />
             </Routes>
-          </Router>
-        </div>
+          </div>
+        </Router>
       </div>
     </div>
   );
