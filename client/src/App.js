166df565af12886c0f3e32c129756963d6ca9fb9
harshit shukla <hharshit8118@gmail.com>
2023-07-18 03:51:28 +0530

    settled routers for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import './app.scss'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
 import Watch from './pages/watch/Watch'
 
 const App = () => {
+  const user = true;
   return (
-    <Home />
-    // <Watch />
-    // <Register />
-    // <Login />
+    <Router>
+      <Routes>
+        <Route path='/' element={user ? <Home />: <Navigate to="/register" />} />
+        <Route path='/register' element={!user ? <Register />: <Navigate to="/" />} />
+        <Route path='/login' element={!user ? <Login />: <Navigate to="/" />} />
+        {user && (<>
+          <Route path='/movies' element={<Home type={'movie'} />} />
+          <Route path='/series' element={<Home type={'series'} />} />
+          <Route path='/watch' element={<Watch /> }/>
+        </>)}
+      </Routes>
+    </Router>
   )
 }
 
