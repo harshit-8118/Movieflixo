dab067f366c18845b1a400d96a8303dce8d197aa
harshit shukla <hharshit8118@gmail.com>
2023-07-24 04:48:40 +0530

    Added validation before login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,18 @@
+import { AuthContext } from "../../context/authContext/AuthContext";
+import { login } from "../../context/authContext/apiCalls";
 import "./login.scss";
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const { dispatch } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  };
   return (
     <div className="login">
       <div className="top">
@@ -14,14 +25,29 @@ const Login = () => {
         </div>
       </div>
       <div className="container">
-       <form>
-        <h1>Sign In</h1>
-        <input type="email" placeholder="Email or phone number" />
-        <input type="password" placeholder="Password" />
-        <button className="loginButton">Sign In</button>
-        <span>New to Netflix? <b>Sign up now.</b></span>
-        <small>This page is protected by google reCAPTCHA to ensure you're not a bot. <b>Learn more</b></small>
-       </form>
+        <form>
+          <h1>Sign In</h1>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="loginButton" onClick={handleLogin}>
+            Sign In
+          </button>
+          <span>
+            New to Netflix? <b>Sign up now.</b>
+          </span>
+          <small>
+            This page is protected by google reCAPTCHA to ensure you're not a
+            bot. <b>Learn more</b>
+          </small>
+        </form>
       </div>
     </div>
   );
