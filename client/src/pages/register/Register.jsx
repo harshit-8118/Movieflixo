7f56866a975fd60cff227f31382492af099b4892
harshit shukla <hharshit8118@gmail.com>
2023-07-24 04:49:33 +0530

    Added username input box, authentication

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -4,17 +4,29 @@ import {
 } from "@mui/icons-material";
 import "./register.scss";
 import { useRef, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
   const emailRef = useRef();
   const passwordRef = useRef();
+  const usernameRef = useRef();
   const handleStart = () => {
     setEmail(emailRef.current.value);
   };
-  const handleFinish = () => {
+  const handleFinish = async (e) => {
+    e.preventDefault();
     setPassword(passwordRef.current.value);
+    setUsername(usernameRef.current.value);
+    try {
+      await axios.post("/auth/register", { email, username, password });
+      navigate("/login");
+    } catch (err) {}
   };
   return (
     <div className="register">
@@ -25,7 +37,9 @@ const Register = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/1200px-Logonetflix.png?20170904093427"
             alt=""
           />
-          <button className="loginButton">Sign In</button>
+          <Link to={"/login"} className="link">
+            <span className="loginButton">Sign in</span>
+          </Link>
         </div>
       </div>
       <div className="container">
@@ -34,11 +48,7 @@ const Register = () => {
         <p>Ready to watch? Enter your to create and start your membership.</p>
         {!email ? (
           <div className="input">
-            <input
-              type="email"
-              placeholder="Email address"
-              ref={emailRef}
-            />
+            <input type="email" placeholder="Email address" ref={emailRef} />
             <button className="registerButton" onClick={handleStart}>
               Get Started
               <span>
@@ -48,11 +58,8 @@ const Register = () => {
           </div>
         ) : (
           <form className="input">
-            <input
-              type="password"
-              ref={passwordRef}
-              placeholder="password"
-            />
+            <input type="text" ref={usernameRef} placeholder="username" />
+            <input type="password" ref={passwordRef} placeholder="password" />
             <button className="registerButton" onClick={handleFinish}>
               Start
             </button>
