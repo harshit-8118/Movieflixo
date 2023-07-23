38f2b0f21d79562233c9aaca714e28e85ad2fc37
harshit shukla <hharshit8118@gmail.com>
2023-07-24 04:51:57 +0530

    managed Authentication, formatting,  and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
-import './app.scss'
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Register from './pages/register/Register'
-import Watch from './pages/watch/Watch'
+import "./app.scss";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import { useContext } from "react";
+import { AuthContext } from "./context/authContext/AuthContext";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-  const user = true;
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <Routes>
-        <Route path='/' element={user ? <Home />: <Navigate to="/register" />} />
-        <Route path='/register' element={!user ? <Register />: <Navigate to="/" />} />
-        <Route path='/login' element={!user ? <Login />: <Navigate to="/" />} />
-        {user && (<>
-          <Route path='/movies' element={<Home type={'movie'} />} />
-          <Route path='/series' element={<Home type={'series'} />} />
-          <Route path='/watch' element={<Watch /> }/>
-        </>)}
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate to="/register" />}
+        />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type={"movie"} />} />
+            <Route path="/series" element={<Home type={"series"} />} />
+            <Route path="/watch" element={<Watch />} />
+          </>
+        )}
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
