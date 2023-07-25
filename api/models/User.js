cd9e1ab1a61a22e190c4bb23c46def05e485b9b1
harshit shukla <hharshit8118@gmail.com>
2023-07-26 03:47:30 +0530

    make view page, register page responsive and navigation profile pic attached

--- api/models/User.js
+++ api/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: {type:String, required: true, unique:true},
     email: {type:String, required: true, unique: true},
     password: {type:String, required: true},
-    profilePic: {type:String, default:""},
+    profilePic: {type:String, default:"https://images.unsplash.com/photo-1579389083078-4e7018379f7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YWRtaW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60"},
     isAdmin: {type:Boolean, default:false}
 }, {timestamps: true});
 
